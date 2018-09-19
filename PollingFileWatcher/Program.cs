3fd6648d6070c36840baed3b9a488d426142d659
Chris Butler <chris.butler@draycir.com>
2018-09-19 09:08:45 +0100

    adding new polling file watcher

---
+++ PollingFileWatcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO.FileSystem.Watcher.Polling;
+using System.Linq;
+
+namespace PollingFileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\draycir\MockData\Signal";
+            MonitorDirectory(path);
+            Console.ReadKey();
+        }
+
+        private static void MonitorDirectory(string path)
+        {
+            PollingFileSystemWatcher fileSystemWatcher = new PollingFileSystemWatcher(path);
+            fileSystemWatcher.ChangedDetailed += FileSystemWatcher_Changed;
+            fileSystemWatcher.Start();
+        }
+
+        private static void FileSystemWatcher_Changed(object sender, PollingFileSystemEventArgs e) =>
+            Console.WriteLine($@"File changed: {string.Join(',', e.Changes.Select(x => $"{x.ChangeType}: {x.Name}"))}");
+    }
+}
